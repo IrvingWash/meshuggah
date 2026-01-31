9ce8e13603e665b5284d7ab4688e243dea508ccc
Washington Irving <iamwashingyou@gmail.com>
2026-01-31 16:57:09 +0400

    matrices

--- modules/Renderer/assets/shaders/test_vertex.glsl
+++ modules/Renderer/assets/shaders/test_vertex.glsl
@@ -4,7 +4,12 @@ layout (location = 0) in vec3 aPosition;
 
 uniform mat4 uModelTransform;
 
+uniform mat4 uProjectionTransform;
+uniform mat4 uViewTransform;
+
 void main()
 {
-    gl_Position = uModelTransform * vec4(aPosition, 1);
+    vec4 worldPosition = uModelTransform * vec4(aPosition, 1);
+
+    gl_Position = uProjectionTransform * uViewTransform * worldPosition;
 }
