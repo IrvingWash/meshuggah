fdb8c6978dddf0bc1282696bb07592e12a1c38ef
Washington Irving <iamwashingyou@gmail.com>
2026-01-31 15:48:16 +0400

    use model transform

--- modules/Renderer/assets/shaders/test_vertex.glsl
+++ modules/Renderer/assets/shaders/test_vertex.glsl
@@ -2,7 +2,9 @@
 
 layout (location = 0) in vec3 aPosition;
 
+uniform mat4 uModelTransform;
+
 void main()
 {
-    gl_Position = vec4(aPosition, 1);
+    gl_Position = uModelTransform * vec4(aPosition, 1);
 }
