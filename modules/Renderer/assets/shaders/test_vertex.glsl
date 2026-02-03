2bb6057a037a8e5d34c59559336d9c1bd6caca9c
Washington Irving <iamwashingyou@gmail.com>
2026-02-03 21:18:17 +0400

    instanced drawing

--- modules/Renderer/assets/shaders/test_vertex.glsl
+++ modules/Renderer/assets/shaders/test_vertex.glsl
@@ -3,8 +3,8 @@
 layout (location = 0) in vec3 aPosition;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aUV;
-
-uniform mat4 uModelTransform;
+layout (location = 3) in vec4 aTint;
+layout (location = 4) in mat4 aModelTransform;
 
 uniform mat4 uProjectionTransform;
 uniform mat4 uViewTransform;
@@ -13,14 +13,16 @@ out FragmentData
 {
     vec3 normal;
     vec2 uv;
+    vec4 tint;
 } fragmentData;
 
 void main()
 {
-    vec4 worldPosition = uModelTransform * vec4(aPosition, 1);
+    vec4 worldPosition = aModelTransform * vec4(aPosition, 1);
 
     gl_Position = uProjectionTransform * uViewTransform * worldPosition;
 
-    fragmentData.normal = transpose(inverse(mat3(uModelTransform))) * aNormal; // @Todo send normal transform into shader
+    fragmentData.normal = transpose(inverse(mat3(aModelTransform))) * aNormal; // @Todo send normal transform into shader
     fragmentData.uv = aUV;
+    fragmentData.tint = aTint;
 }
